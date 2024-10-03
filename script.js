908f97ac7442a8935ccd57b9ee8a18b552599450
Captain-Ok <loneranger2520@gmail.com>
2024-10-03 11:21:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function takeCommand(message) {
         speak("My Name is Aura")
     } 
     else if (message.includes("who are you")) {
-        speak("I am a virtual assistant.");
+        speak("I am a virtual assistant created by Akhil sir.");
         recognition.stop();
     } else if (message.includes("open youtube")) {
         speak("opening youtube")
